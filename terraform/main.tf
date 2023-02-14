a343c2f8ef0f20580627f79620dd5aa7e5d2c742
Milky19 <97377003+Milky19@users.noreply.github.com>
2023-02-14 11:58:43 +0545

    Delete terraform directory

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider  "aws" {
-  region        = "ap-south-1"
-  access_key    = "AKIAWO5LHJ53MQ6N6AQR"
-  secret_kwy    = "nYehaRf5SDXWxFAD++jHdt8J/islkFT4IlZD4IGC"
-}
-
-resource  "aws_s3_bucket"  "example2" {
-   bucket        = "krishna908"
-}
