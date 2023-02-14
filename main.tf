d90c1a00d27fab0c0a153c21925519c5e13eed7d
Milky19 <97377003+Milky19@users.noreply.github.com>
2023-02-14 16:30:05 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider  "aws" {
   region        = "ap-south-1"
   access_key    = "AKIAWO5LHJ53MQ6N6AQR"
-  secret_kwy    = "nYehaRf5SDXWxFAD++jHdt8J/islkFT4IlZD4IGC"
+  secret_key    = "nYehaRf5SDXWxFAD++jHdt8J/islkFT4IlZD4IGC"
 }
 
 resource  "aws_s3_bucket"  "example2" {
